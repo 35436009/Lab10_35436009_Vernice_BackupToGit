1bfd337848cebbe5b9750224d2c03b2291ca990e
35436009_VerniceFoong <vnvirusread@gmail.com>
2026-04-03 20:13:41 +0800

    Update WeatherRec.cpp

--- WeatherRec.cpp
+++ WeatherRec.cpp
@@ -1,187 +1,147 @@
 #include "WeatherRec.h"
 
-// Default constructor.
+// Default constructor
 WeatherRec::WeatherRec()
 {
-    m_date = Date();
-    m_time = Time();
     m_speed = 0.0;
     m_solarRadiation = 0.0;
     m_temperature = 0.0;
+
     m_hasSpeed = false;
     m_hasSolar = false;
     m_hasTemp = false;
 }
 
-// Parameterized constructor.
+// Constructor with Date and Time
 WeatherRec::WeatherRec(const Date& date, const Time& time)
 {
     m_date = date;
     m_time = time;
+
     m_speed = 0.0;
     m_solarRadiation = 0.0;
     m_temperature = 0.0;
+
     m_hasSpeed = false;
     m_hasSolar = false;
     m_hasTemp = false;
 }
 
-// Get the date.
-const Date& WeatherRec::GetDate() const
+// Get Date
+Date WeatherRec::GetDate() const
 {
     return m_date;
 }
 
-// Sets the date.
-void WeatherRec::SetDate(const Date& date)
+// Set Date
+void WeatherRec::SetDate(const Date & date)
 {
     m_date = date;
 }
 
-// Get the time.
-const Time& WeatherRec::GetTime() const
+// Get Time
+Time WeatherRec::GetTime() const
 {
     return m_time;
 }
 
-// Sets the time.
-void WeatherRec::SetTime(const Time& time)
+// Set Time
+void WeatherRec::SetTime(const Time & time)
 {
     m_time = time;
 }
 
-// Check whether wind speed is available.
+// Wind
 bool WeatherRec::HasSpeed() const
 {
     return m_hasSpeed;
 }
 
-// Gets wind speed.
+// Get Speed
 double WeatherRec::GetSpeed() const
 {
     return m_speed;
 }
 
-// Sets wind speed and validity.
+// Set Speed
 void WeatherRec::SetSpeed(double speed, bool valid)
 {
     m_speed = speed;
     m_hasSpeed = valid;
 }
 
-// Check whether solar radiation is available.
+// Solar Radiation
 bool WeatherRec::HasSolar() const
 {
     return m_hasSolar;
 }
 
-// Gets solar radiation.
+// Get Solar Radiation
 double WeatherRec::GetSolarRadiation() const
 {
     return m_solarRadiation;
 }
 
-// Sets solar radiation and validity.
+// Set Solar Radiation
 void WeatherRec::SetSolarRadiation(double solar, bool valid)
 {
     m_solarRadiation = solar;
     m_hasSolar = valid;
 }
 
-// Check whether temperature is available.
+// Temperature
 bool WeatherRec::HasTemp() const
 {
     return m_hasTemp;
 }
 
-// Gets temperature.
+// Get Temp
 double WeatherRec::GetTemperature() const
 {
     return m_temperature;
 }
 
-// Sets temperature and validity.
-void WeatherRec::SetTemperature(double temperature, bool valid)
+// Set Temp
+void WeatherRec::SetTemperature(double temp, bool valid)
 {
-    m_temperature = temperature;
+    m_temperature = temp;
     m_hasTemp = valid;
 }
 
-// Compares two weather records by timestamp.
-bool WeatherRec::operator<(const WeatherRec& other) const
-{
-    if (m_date < other.m_date)
-    {
-        return true;
-    }
-    else if (m_date > other.m_date)
-    {
-        return false;
-    }
-    else
-    {
-        if (m_time < other.m_time)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-}
-
-// Checks whether two records have the same timestamp.
-bool WeatherRec::operator==(const WeatherRec& other) const
-{
-    if (m_date == other.m_date)
-    {
-        if (m_time == other.m_time)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    else
-    {
-        return false;
-    }
-}
-
-// Compares two weather records by timestamp.
-bool WeatherRec::operator>(const WeatherRec& other) const
-{
-    if (m_date > other.m_date)
-    {
-        return true;
-    }
-    else if (m_date < other.m_date)
-    {
-        return false;
-    }
-    else
-    {
-        if (m_time > other.m_time)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-}
-
-// Outputs a weather record to a stream.
+// Stream output so WeatherRec can be printed
 std::ostream& operator<<(std::ostream& os, const WeatherRec& rec)
 {
-    os << rec.GetDate() << " " << rec.GetTime();
-    os << " Speed=" << rec.GetSpeed();
-    os << " Solar=" << rec.GetSolarRadiation();
-    os << " Temp=" << rec.GetTemperature();
+    os << rec.GetDate() << " " << rec.GetTime() << " | ";
+
+    os << "Speed=";
+    if (rec.HasSpeed())
+    {
+        os << rec.GetSpeed();
+    }
+    else
+    {
+        os << "N/A";
+    }
+
+    os << ", Solar=";
+    if (rec.HasSolar())
+    {
+        os << rec.GetSolarRadiation();
+    }
+    else
+    {
+        os << "N/A";
+    }
+
+    os << ", Temp=";
+    if (rec.HasTemp())
+    {
+        os << rec.GetTemperature();
+    }
+    else
+    {
+        os << "N/A";
+    }
 
     return os;
 }

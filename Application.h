fdc2ffa01e9241942900ebd40c23a3d517003020
35436009_VerniceFoong <vnvirusread@gmail.com>
2026-04-03 19:56:12 +0800

    Update Application.h

--- Application.h
+++ Application.h
@@ -2,87 +2,31 @@
 #define APPLICATION_H
 
 #include "WeatherLog.h"
+#include <map>
+#include "BST.h"
 
-/**
- * @class Application
- * @brief Controls the menu system and program execution.
- *
- * Responsibilities:
- * - Display menu
- * - Handle user input
- * - Call statistics functions
- * - Write output file
- *
- * @author Vernice Foong
- * @version 06
- */
 class Application
 {
 public:
-    /**
-     * @brief Constructor that uses the loaded WeatherLog.
-     *
-     * @param log Reference to the weather log.
-     */
     Application(WeatherLog& log);
-
-    /**
-     * @brief Starts the program and controls the main menu loop.
-     */
     void Run();
 
 private:
-    WeatherLog& m_log;  // Reference to the loaded WeatherLog.
+    WeatherLog& m_log;
 
-    /**
-     * @brief Displays the main menu options to the user.
-     */
+
+    std::map<int, std::multimap<int, WeatherRec> > m_dataMap;
+    BST<int> m_tree;
+
     void DisplayMenu() const;
-
-    /**
-     * @brief Reads an integer input from the user.
-     *
-     * @return The integer entered by the user.
-     */
     int ReadInt() const;
-
-    /**
-     * @brief Returns the English name of a given month.
-     *
-     * @param month Month number (1-12).
-     * @return Pointer to a constant month name string.
-     */
     const char* GetMonthName(int month) const;
 
-    /**
-     * @brief Menu Option 1:
-     * Computes and displays wind speed mean and sample standard deviation
-     * for a specified month and year.
-     */
     void DoOption1();
-
-    /**
-     * @brief Menu Option 2:
-     * Computes and displays temperature mean and sample standard deviation
-     * for each month of a specified year.
-     */
     void DoOption2();
-
-    /**
-     * @brief Menu Option 3:
-     * Computes and displays sample Pearson correlation coefficients for
-     * wind speed, temperature, and solar radiation for a specified month.
-     */
     void DoOption3();
-
-    /**
-     * @brief Menu Option 4:
-     * Generates WindTempSolar.csv containing monthly wind speed, temperature,
-     * and solar radiation statistics for a specified year.
-     * Output includes average, sample standard deviation, and mean absolute deviation
-     * for wind speed and temperature.
-     */
     void DoOption4();
+
 };
 
 #endif
